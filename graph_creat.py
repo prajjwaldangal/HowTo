119e663ef016e1893ee561685638df695a822ba7
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-22 12:02:28 -0500

    "Updated properly"

--- graph_creat.py
+++ graph_creat.py
@@ -1,36 +1,20 @@
-# from nltk.parse.stanford import StanfordDependencyParser
-# path1 = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser.jar'
-# path2 = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar'
-# dependency_parser = StanfordDependencyParser(path_to_jar=path1, path_to_models_jar=path2)
-
-# from nltk.parse.stanford import StanfordParser
-# # list(parser.raw_parse("the quick brown fox jumps over the lazy dog"))
-# from nltk.internals import find_jars_within_path
-# jar = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser.jar'
-# model = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar'
-# parser=StanfordParser(model,jar)
-
-# import os
-# from nltk.parse import stanford
-# os.environ['STANFORD_PARSER'] = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser.jar'
-# os.environ['STANFORD_MODELS'] = 'Users/Rouzbeh/Downloads/Hackathon/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar'
-# parser = stanford.StanfordParser(model_path="Users/Rouzbeh/Downloads/Hackathon/englishPCFG.ser.gz")
-# sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your name?"))
-# print sentences
-
 from graphviz import Digraph
 import nltk
 from nltk.parse.stanford import StanfordParser
+from string import ascii_lowercase
+
 path_to_jar = 'stanford-parser-full-2016-10-31/stanford-parser.jar'
 path_to_models_jar = 'stanford-parser-full-2016-10-31/stanford-parser-3.7.0-models.jar'
 parser = StanfordParser(path_to_jar=path_to_jar, path_to_models_jar=path_to_models_jar)
 
-
-text_doc = 'First take out your jumper cables and then place both vehicles in park and shut off the engine. Attach the clip to the positive terminal of your battery and then attach the other red clip to the positive terminal of the other battery. Attach one of the black clip to the negative terminal of the battery. Attach the other last black clip to any metal surface in the car that is not a battery. start the working car and let it run for few minutes. Try to start your car. Remove the clips in the reverse order.'
+print "The program is running, please wait !"
+text_doc = 'Attend every class sessions. Pay attention and take good notes. Review your notes after every class. Always turn homeworks on time. Study for exam a week in advance. Get a enough sleep and eat a good meal before taking the exam. Good luck with the exam and get ready for the parties.'
 sents = nltk.sent_tokenize(text_doc)
+all_list = []
 for sent in sents:
 	result = parser.raw_parse(sent)
 	final = list(result)
+	#print final[0].pretty_print()
 	my_list = []
 	flag2 = flag1 = 0
 	string = ""
@@ -69,14 +53,34 @@ for sent in sents:
 	if string != "":
 		my_list.append(string)
 	del my_list[0]
-	print my_list
+	all_list.append(my_list)
+	#print my_list
 
+dot = Digraph(comment='The Round Table')
+chars = []
+index = 0
+for alpha in ascii_lowercase:
+	chars.append(alpha)
+edge_list = []
+for item in reversed(all_list):
+	local_char = []
+	for elem in item:
+		local_char.append(chars[index])
+		dot.node(chars[index], elem)
+		index += 1
+	i = 0
+	
+	while i < (len (local_char) -1):
+		edge_list.append(local_char[i]+local_char[i+1])
+		i +=1 
+dot.edges(edge_list)
 	# dot = Digraph(comment='The Round Table')
 	# dot.node('A', my_list[0])
 	# dot.node('B', my_list[1])
 	# dot.node('C', my_list[2])
 	# dot.edges(['AB','BC'])
-
-	# dot.render('test-output/round-table.gv', view=True)
+dot.graph_attr['rankdir'] = 'LR'
+dot.graph_attr['color'] = 'red'
+dot.render('test-output/round-table.gv', view=True)
 
 
